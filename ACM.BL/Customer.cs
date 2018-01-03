dbc587159b1e9ae1137dfc2eb5847e01062d364a
Jay <jdesai738@gmail.com>
2018-01-03 00:15:19 -0800

    Added Tests

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -34,7 +34,7 @@ namespace ACM.BL
         {
             get
             {
-                return LastName + "," + FirstName;
+                return LastName + ", " + FirstName;
             }
         }
 
---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameValid()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            customer.LastName = "Baggins";
+
+            var expected = "Baggins, Bilbo";
+
+            //--Act
+            var actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
